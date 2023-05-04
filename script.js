ffcc4611160ca184332ccf0579f7f4d6905359ab
Lyuba Bessarab <bessarab97@gmail.com>
2023-05-05 00:36:34 +0300

    fixed errors

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-const input = document.querySelector(".search");
-const varsContainer = document.querySelector(".varsContainer");
-const searchVariant = document.querySelector(".var");
+const input = document.querySelector(".search"); // для облегчения проверку добавила комменты к изменениям, выделила стили в отдельный файл, переименовала некоторые переменные
+const outputContainer = document.querySelector(".outputContainer");
 const list = document.querySelector(".list");
 
 function getData(inputValue) {
@@ -12,37 +11,28 @@ function getData(inputValue) {
 }
 
 function onChange(e) {
-  varsContainer.textContent = null;
+  outputContainer.textContent = null;
   let value = e.target.value;
   if (value.length === 0) {
-    varsContainer.textContent = null;
+    outputContainer.textContent = null;
     return;
   }
-  if (value === " ") return;
+  if (!value.trim().length > 0) return; // поменяла условие
   getData(value)
     .then((data) => {
       const arr = data.items;
       for (let item of arr) {
-        let objRes = {
-          name: item.description,
-          stars: item.stargazers_count,
-          owner: item.owner.login,
+        let repInfo = {
+          name: item.name ? item.name : "неизвестно", //поменяла item.description на item.name но изначально был description потому что в поиске тогда выдавалось название репозитория с теми словами, которые вбиваются в поиске. с name названия в выдаче не всегда совпадают с тем, что ищешь
+          stars: item.stargazers_count ? item.stargazers_count : "неизвестно",
+          owner: item.owner.login ? item.name : "неизвестно",
         };
-        createVar(objRes);
+        createOutput(repInfo);
       }
     })
     .catch((err) => {
-      let error = createEl("div", "error", err.description);
-      input.insertAdjacentElement("afterend", error);
       console.log(err);
     });
-
-  function createEl(tag, className, value) {
-    const el = document.createElement(tag);
-    el.classList.add(className);
-    el.textContent = value;
-    return el;
-  }
 }
 
 onChange = debounce(onChange, 500);
@@ -60,34 +50,35 @@ function debounce(fn, ms) {
   };
 }
 
-function createEl(tag, className, value) {
+function createElement(tag, className, value) {
   const el = document.createElement(tag);
   el.classList.add(className);
   el.textContent = value;
   return el;
 }
 
-function createVar(obj) {
-  let searchVariant = createEl("div", "var", obj.name);
+function createOutput(obj) {
+  let output = createElement("div", "output", obj.name);
 
-  if (searchVariant.textContent.length > 30) {
-    searchVariant.textContent = searchVariant.textContent.slice(0, 30) + "...";
+  if (output.textContent.length > 30) {
+    output.textContent = searchVariant.textContent.slice(0, 30) + "...";
   }
-  searchVariant.addEventListener("click", () => {
+  output.addEventListener("click", function addToList() {
     createItem(obj);
     input.value = "";
-    varsContainer.textContent = null;
+    outputContainer.textContent = null;
+    output.removeEventListener("click", addToList); //удалила обработчик 1
   });
-  varsContainer.insertAdjacentElement("beforeend", searchVariant);
+  outputContainer.insertAdjacentElement("beforeend", output);
 }
 
 function createItem(obj) {
-  let item = createEl("li", "item"),
-    info = createEl("div", "info"),
-    btn = createEl("button", "del"),
-    name = createEl("p", "info__data", "Name: " + obj.name),
-    owner = createEl("p", "info__data", "Owner: " + obj.owner),
-    stars = createEl("p", "info__data", "Stars: " + obj.stars);
+  let item = createElement("li", "item"),
+    info = createElement("div", "info"),
+    btn = createElement("button", "del"),
+    name = createElement("p", "info__data", "Name: " + obj.name),
+    owner = createElement("p", "info__data", "Owner: " + obj.owner),
+    stars = createElement("p", "info__data", "Stars: " + obj.stars);
   info.insertAdjacentElement("beforeend", name);
   info.insertAdjacentElement("beforeend", owner);
   info.insertAdjacentElement("beforeend", stars);
@@ -98,7 +89,8 @@ function createItem(obj) {
   if (name.textContent.length > 30) {
     name.textContent = item.textContent.slice(0, 30) + "...";
   }
-  btn.addEventListener("click", function (e) {
+  btn.addEventListener("click", function removeItem() {
     item.remove();
+    btn.removeEventListener("click", removeItem); //удалила обработчик 2
   });
 }
